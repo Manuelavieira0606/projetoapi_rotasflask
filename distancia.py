5658f6774ea8e94c931acfeb5fed7c1eaade2034
Manuelavieira0606 <manuela.rvieira09@gmail.com>
2024-04-29 15:10:42 -0300

    Pedir nome para o usuario

--- distancia.py
+++ distancia.py
@@ -3,4 +3,5 @@ destino = input("Cidade de destino: ")
 distancia = float(input("Qual a distancia em km? "))
 velocidade = float(input("Quantos km/h irá viajar? "))
 tempo = distancia/velocidade*60
-print(f"Você levará {tempo} minutos de {origem} à {destino} ")
+nome_motorista = input("Digite o nome do motorista: ")
+print(f"{nome_motorista} você levará {tempo} minutos de {origem} à {destino} ")
